4b862bf2b675c45a310500a559a27ded613fa185
Kyle Robinson <kr604126@digicelgroup.com>
2020-12-03 09:44:06 -0500

    Add project files.

---
+++ CosmosAPI/Controllers/itemEntryController.cs
@@ -0,0 +1,77 @@
+﻿using CosmosAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class itemEntryController : ControllerBase
+    {
+        private readonly IDocumentClient _documentClient;
+        readonly string databaseId;
+        readonly string collectionId;
+        public IConfiguration Configuration { get; }
+
+        public itemEntryController(IDocumentClient documentClient, IConfiguration configuration)
+        {
+            _documentClient = documentClient;
+            Configuration = configuration;
+
+            databaseId = Configuration["DatabaseId"];
+            collectionId = "itemEntry";
+
+            BuildCollection().Wait();
+        }
+
+        private async Task BuildCollection()
+        {
+            await _documentClient.CreateDatabaseIfNotExistsAsync(new Database { Id = databaseId });
+            await _documentClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(databaseId),
+            new DocumentCollection { Id = collectionId }); 
+        }
+
+        [HttpGet]
+        public IQueryable<itemEntry> Get()
+        {
+            return _documentClient.CreateDocumentQuery<itemEntry>(UriFactory.CreateDocumentCollectionUri(databaseId, collectionId));
+        }
+
+        [HttpGet("{id}")]
+        public IQueryable<itemEntry> Get(string id)
+        {
+            return _documentClient.CreateDocumentQuery<itemEntry>(UriFactory.CreateDocumentCollectionUri(databaseId, collectionId), 
+                new FeedOptions { MaxItemCount = 1 }).Where((i) => i.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] itemEntry item)
+        {
+            var response = await _documentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseId, collectionId), item);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] itemEntry item)
+        {
+            await _documentClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId, collectionId, id),item);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _documentClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseId, collectionId, id));
+            return Ok();
+        }
+
+    }
+
+}
---
+++ CosmosAPI/Models/itemEntry.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosAPI.Models
+{
+    public partial class itemEntry
+    {
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public string Id { get; set; }
+
+        [JsonProperty("comment", NullValueHandling = NullValueHandling.Ignore)]
+        public string Comment { get; set; }
+
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("request_attachments", NullValueHandling = NullValueHandling.Ignore)]
+        public RequestAttachments RequestAttachments { get; set; }
+
+        [JsonProperty("request_properties", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, object> RequestProperties { get; set; }
+
+        [JsonProperty("request_status", NullValueHandling = NullValueHandling.Ignore)]
+        public string RequestStatus { get; set; }
+
+        [JsonProperty("request_type", NullValueHandling = NullValueHandling.Ignore)]
+        public string RequestType { get; set; }
+
+        [JsonProperty("scheduled_date_time", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? ScheduledDateTime { get; set; }
+
+        [JsonProperty("source", NullValueHandling = NullValueHandling.Ignore)]
+        public string Source { get; set; }
+
+        [JsonProperty("submit_date_time", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? SubmitDateTime { get; set; }
+
+        [JsonProperty("updatedAt", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? UpdatedAt { get; set; }
+    }
+
+    public partial class RequestAttachments
+    {
+        [JsonProperty("input", NullValueHandling = NullValueHandling.Ignore)]
+        public object[] Input { get; set; }
+
+        [JsonProperty("output", NullValueHandling = NullValueHandling.Ignore)]
+        public object[] Output { get; set; }
+    }
+}
